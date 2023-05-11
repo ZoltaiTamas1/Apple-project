611dd43ce261609f91bc112c6964ad86732eba89
Dávid Pop <pop.david.tibor@students.jedlik.eu>
2023-05-11 21:04:49 +0200

    segítség újragondolás

---
+++ README.md
@@ -0,0 +1,13 @@
+# Apple
+
+Tomi
+-index.html
+-iphone.html
+-ipad.html
+-kiegeszitok.html
+
+Dávid
+-watch.html
+-macbook.html
+-segitseg.html
+-js
---
+++ egyesitett.js
@@ -0,0 +1,240 @@
+let hatterszam = 0  
+let shopping = 0
+function Szinvaltas1(){
+    hatterszam += 1
+    if (hatterszam == 4){
+        hatterszam = 0
+    }
+}
+function Szinvaltas2(){
+    if (hatterszam == 0){
+        document.body.style.backgroundImage = "url('pics/Hatterkepek/bg-4.jpg')";
+    }
+    if (hatterszam == 1){
+        document.body.style.backgroundImage = "url('pics/Hatterkepek/bg-5.jpg')";
+    }
+    if (hatterszam == 2){
+        document.body.style.backgroundImage = "url('pics/Hatterkepek/bg-6.jpg')";
+    }
+    if (hatterszam == 3){
+        document.body.style.backgroundImage = "url('pics/Hatterkepek/bg-7.jpg')";
+    }
+}
+function shopping1(){
+    if (shopping == 0){
+        document.getElementById('lista1').innerHTML = 'iphone 14'
+        shopping += 1
+    } else if (shopping == 1){
+        document.getElementById('lista2').innerHTML = 'iphone 14'
+        shopping += 1
+    } else if (shopping == 2){
+        document.getElementById('lista3').innerHTML = 'iphone 14'
+        shopping += 1
+    } else if (shopping == 3){
+        document.getElementById('lista4').innerHTML = 'iphone 14'
+        shopping += 1
+    } else if (shopping == 4){
+        document.getElementById('lista5').innerHTML = 'iphone 14'
+        shopping += 1
+    } else if (shopping == 5){
+        document.getElementById('lista6').innerHTML = 'iphone 14'
+        shopping += 1
+        shopping = 0
+    }
+ 
+    console.log(hatterszam)
+}
+function shopping2(){
+    if (shopping == 0){
+        document.getElementById('lista1').innerHTML = 'iphone 13'
+        shopping += 1
+    } else if (shopping == 1){
+        document.getElementById('lista2').innerHTML = 'iphone 13'
+        shopping += 1
+    } else if (shopping == 2){
+        document.getElementById('lista3').innerHTML = 'iphone 13'
+        shopping += 1
+    } else if (shopping == 3){
+        document.getElementById('lista4').innerHTML = 'iphone 13'
+        shopping += 1
+    } else if (shopping == 4){
+        document.getElementById('lista5').innerHTML = 'iphone 13'
+        shopping += 1
+    } else if (shopping == 5){
+        document.getElementById('lista6').innerHTML = 'iphone 13'
+        shopping += 1
+        shopping = 0
+    }
+ 
+    console.log(hatterszam)
+}
+function shopping3(){
+    if (shopping == 0){
+        document.getElementById('lista1').innerHTML = 'Ipad'
+        shopping += 1
+    } else if (shopping == 1){
+        document.getElementById('lista2').innerHTML = 'Ipad'
+        shopping += 1
+    } else if (shopping == 2){
+        document.getElementById('lista3').innerHTML = 'Ipad'
+        shopping += 1
+    } else if (shopping == 3){
+        document.getElementById('lista4').innerHTML = 'Ipad'
+        shopping += 1
+    } else if (shopping == 4){
+        document.getElementById('lista5').innerHTML = 'Ipad'
+        shopping += 1
+    } else if (shopping == 5){
+        document.getElementById('lista6').innerHTML = 'Ipad'
+        shopping += 1
+        shopping = 0
+    }
+ 
+    console.log(hatterszam)
+}
+function shopping4(){
+    if (shopping == 0){
+        document.getElementById('lista1').innerHTML = 'Ipad Pro'
+        shopping += 1
+    } else if (shopping == 1){
+        document.getElementById('lista2').innerHTML = 'Ipad Pro'
+        shopping += 1
+    } else if (shopping == 2){
+        document.getElementById('lista3').innerHTML = 'Ipad Pro'
+        shopping += 1
+    } else if (shopping == 3){
+        document.getElementById('lista4').innerHTML = 'Ipad Pro'
+        shopping += 1
+    } else if (shopping == 4){
+        document.getElementById('lista5').innerHTML = 'Ipad Pro'
+        shopping += 1
+    } else if (shopping == 5){
+        document.getElementById('lista6').innerHTML = 'Ipad Pro'
+        shopping += 1
+        shopping = 0
+    }
+    
+ 
+    console.log(hatterszam)
+}
+function shopping5(){
+    if (shopping == 0){
+        document.getElementById('lista1').innerHTML = 'Ipad Air'
+        shopping += 1
+    } else if (shopping == 1){
+        document.getElementById('lista2').innerHTML = 'Ipad Air'
+        shopping += 1
+    } else if (shopping == 2){
+        document.getElementById('lista3').innerHTML = 'Ipad Air'
+        shopping += 1
+    } else if (shopping == 3){
+        document.getElementById('lista4').innerHTML = 'Ipad Air'
+        shopping += 1
+    } else if (shopping == 4){
+        document.getElementById('lista5').innerHTML = 'Ipad Air'
+        shopping += 1
+    } else if (shopping == 5){
+        document.getElementById('lista6').innerHTML = 'Ipad Air'
+        shopping += 1
+        shopping = 0
+    }
+}
+function shopping6(){
+    if (shopping == 0){
+        document.getElementById('lista1').innerHTML = 'Macbook Pro'
+        shopping += 1
+    } else if (shopping == 1){
+        document.getElementById('lista2').innerHTML = 'Macbook Pro'
+        shopping += 1
+    } else if (shopping == 2){
+        document.getElementById('lista3').innerHTML = 'Macbook Pro'
+        shopping += 1
+    } else if (shopping == 3){
+        document.getElementById('lista4').innerHTML = 'Macbook Pro'
+        shopping += 1
+    } else if (shopping == 4){
+        document.getElementById('lista5').innerHTML = 'Macbook Pro'
+        shopping += 1
+    } else if (shopping == 5){
+        document.getElementById('lista6').innerHTML = 'Macbook Pro'
+        shopping += 1
+        shopping = 0
+    }
+}
+function shopping7(){
+    if (shopping == 0){
+        document.getElementById('lista1').innerHTML = 'Macbook Air'
+        shopping += 1
+    } else if (shopping == 1){
+        document.getElementById('lista2').innerHTML = 'Macbook Air'
+        shopping += 1
+    } else if (shopping == 2){
+        document.getElementById('lista3').innerHTML = 'Macbook Air'
+        shopping += 1
+    } else if (shopping == 3){
+        document.getElementById('lista4').innerHTML = 'Macbook Air'
+        shopping += 1
+    } else if (shopping == 4){
+        document.getElementById('lista5').innerHTML = 'Macbook Air'
+        shopping += 1
+    } else if (shopping == 5){
+        document.getElementById('lista6').innerHTML = 'Macbook Air'
+        shopping += 1
+        shopping = 0
+    }
+}
+function shopping8(){
+    if (shopping == 0){
+        document.getElementById('lista1').innerHTML = 'Watch Ultra'
+        shopping += 1
+    } else if (shopping == 1){
+        document.getElementById('lista2').innerHTML = 'Watch Ultra'
+        shopping += 1
+    } else if (shopping == 2){
+        document.getElementById('lista3').innerHTML = 'Watch Ultra'
+        shopping += 1
+    } else if (shopping == 3){
+        document.getElementById('lista4').innerHTML = 'Watch Ultra'
+        shopping += 1
+    } else if (shopping == 4){
+        document.getElementById('lista5').innerHTML = 'Watch Ultra'
+        shopping += 1
+    } else if (shopping == 5){
+        document.getElementById('lista6').innerHTML = 'Watch Ultra'
+        shopping += 1
+        shopping = 0
+    }
+}
+function shopping9(){
+    if (shopping == 0){
+        document.getElementById('lista1').innerHTML = 'Watch SE'
+        shopping += 1
+    } else if (shopping == 1){
+        document.getElementById('lista2').innerHTML = 'Watch SE'
+        shopping += 1
+    } else if (shopping == 2){
+        document.getElementById('lista3').innerHTML = 'Watch SE'
+        shopping += 1
+    } else if (shopping == 3){
+        document.getElementById('lista4').innerHTML = 'Watch SE'
+        shopping += 1
+    } else if (shopping == 4){
+        document.getElementById('lista5').innerHTML = 'Watch SE'
+        shopping += 1
+    } else if (shopping == 5){
+        document.getElementById('lista6').innerHTML = 'Watch SE'
+        shopping += 1
+        shopping = 0
+    }
+}
+function torles(){
+    shopping = 0
+    document.getElementById('lista1').innerHTML = ''
+    document.getElementById('lista2').innerHTML = ''
+    document.getElementById('lista3').innerHTML = ''
+    document.getElementById('lista4').innerHTML = ''
+    document.getElementById('lista5').innerHTML = ''
+    document.getElementById('lista6').innerHTML = ''
+
+}
+console.log(shopping)
