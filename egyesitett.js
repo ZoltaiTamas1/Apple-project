c65987bb3c752a41451d317132746145246a199f
Dávid Pop <pop.david.tibor@students.jedlik.eu>
2023-05-14 20:03:47 +0200

    végső simítások

--- egyesitett.js
+++ egyesitett.js
@@ -95,17 +95,169 @@ function vasarlas2(){
     document.getElementById('osszesar').innerHTML = 'Ár: ' + ar
 }
 function vasarlas3(){
-    let item = document.getElementById('segitsegselectipadpro').value
+    let item = document.getElementById('segitsegselectipadpro').value;
 
-if (item == '11 huvelykes' || item == '11 hüvelykes') {
-    ar += 469990;
-    item = '11 hüvelykes Ipad Pro'
-} else if (item == '12.9 huvelykes' || item == '12.9 hüvelykes') {
-    ar += 649990;
-    item = '12.9 hüvelykes Ipad Pro'
+    if (item == '11 huvelykes' || item == '11 hüvelykes') {
+        ar += 469990;
+        item = '11 hüvelykes';
+    } else if (item == '12.9 huvelykes' || item == '12.9 hüvelykes') {
+        ar += 649990;
+        item = '12.9 hüvelykes';
+    }
+    listaszam += 1
+    if (listaszam == 1){
+        lista1.style.visibility = 'visible'
+        lista1.innerHTML = item
+    }
+    if (listaszam == 2){
+        lista2.style.visibility = 'visible'
+        lista2.innerHTML = item
+    }
+    if (listaszam == 3){
+        lista3.style.visibility = 'visible'
+        lista3.innerHTML = item
+    }
+    if (listaszam == 4){
+        lista4.style.visibility = 'visible'
+        lista4.innerHTML = item
+    }
+    if (listaszam == 5){
+        lista5.style.visibility = 'visible'
+        lista5.innerHTML = item
+    }
+    if (listaszam == 6){
+        lista6.style.visibility = 'visible'
+        lista6.innerHTML = item
+        listaszam = 0
+    }
+    console.log(listaszam)
+    document.getElementById('osszesar').innerHTML = 'Ár: ' + ar
 }
+function vasarlas4(){
+    let item = document.getElementById('segitsegselectipadair').value;
 
-    
+    if (item == '10.9 huvelykes' || item == '10.9 hüvelykes') {
+        ar += 469990;
+        item = '10.9 hüvelykes';
+    }
+    if (item == '12 huvelykes' || item == '12 hüvelykes') {
+        ar += 469990;
+        item = '12 hüvelykes';
+    }
+    listaszam += 1
+    if (listaszam == 1){
+        lista1.style.visibility = 'visible'
+        lista1.innerHTML = item
+    }
+    if (listaszam == 2){
+        lista2.style.visibility = 'visible'
+        lista2.innerHTML = item
+    }
+    if (listaszam == 3){
+        lista3.style.visibility = 'visible'
+        lista3.innerHTML = item
+    }
+    if (listaszam == 4){
+        lista4.style.visibility = 'visible'
+        lista4.innerHTML = item
+    }
+    if (listaszam == 5){
+        lista5.style.visibility = 'visible'
+        lista5.innerHTML = item
+    }
+    if (listaszam == 6){
+        lista6.style.visibility = 'visible'
+        lista6.innerHTML = item
+        listaszam = 0
+    }
+    console.log(listaszam)
+    document.getElementById('osszesar').innerHTML = 'Ár: ' + ar
+}
+function vasarlas5(){
+    let item = document.getElementById('segitsegselectipad').value;
+    alert(item)
+    if (item == '10.9 huvelykes' || item == '10.9 hüvelykes') {
+        ar += 259990;
+        item = '10.9 hüvelykes';
+    }
+    if (item == '12 huvelykes' || item == '12 hüvelykes') {
+        ar += 399990;
+        item = '12 hüvelykes';
+    }
+    listaszam += 1
+    if (listaszam == 1){
+        lista1.style.visibility = 'visible'
+        lista1.innerHTML = item
+    }
+    if (listaszam == 2){
+        lista2.style.visibility = 'visible'
+        lista2.innerHTML = item
+    }
+    if (listaszam == 3){
+        lista3.style.visibility = 'visible'
+        lista3.innerHTML = item
+    }
+    if (listaszam == 4){
+        lista4.style.visibility = 'visible'
+        lista4.innerHTML = item
+    }
+    if (listaszam == 5){
+        lista5.style.visibility = 'visible'
+        lista5.innerHTML = item
+    }
+    if (listaszam == 6){
+        lista6.style.visibility = 'visible'
+        lista6.innerHTML = item
+        listaszam = 0
+    }
+    console.log(listaszam)
+    document.getElementById('osszesar').innerHTML = 'Ár: ' + ar
+}
+function vasarlas6(){
+    let item = document.getElementById('segitsegselectmacair').value;
+    if (item == 'Macbook air M1') {
+        ar += 499990;
+    }
+    if (item == 'Macbook air M2') {
+        ar += 599990;
+    }
+    listaszam += 1
+    if (listaszam == 1){
+        lista1.style.visibility = 'visible'
+        lista1.innerHTML = item
+    }
+    if (listaszam == 2){
+        lista2.style.visibility = 'visible'
+        lista2.innerHTML = item
+    }
+    if (listaszam == 3){
+        lista3.style.visibility = 'visible'
+        lista3.innerHTML = item
+    }
+    if (listaszam == 4){
+        lista4.style.visibility = 'visible'
+        lista4.innerHTML = item
+    }
+    if (listaszam == 5){
+        lista5.style.visibility = 'visible'
+        lista5.innerHTML = item
+    }
+    if (listaszam == 6){
+        lista6.style.visibility = 'visible'
+        lista6.innerHTML = item
+        listaszam = 0
+    }
+    console.log(listaszam)
+    document.getElementById('osszesar').innerHTML = 'Ár: ' + ar
+}
+function vasarlas7(){
+    let item = document.getElementById('segitsegselectmacpro').value;
+    if (item == '13 huvelykes') {
+        ar += 649990;
+    }
+    if (item == '14 huvelykes') {
+        ar += 999990;
+    }
     listaszam += 1
     if (listaszam == 1){
         lista1.style.visibility = 'visible'
@@ -136,3 +288,40 @@ if (item == '11 huvelykes' || item == '11 hüvelykes') {
     document.getElementById('osszesar').innerHTML = 'Ár: ' + ar
 }
 
+function vasarlas8(){
+    let item = document.getElementById('segitsegselectwatch1').value;
+    if (item == 'Watch Ultra') {
+        ar += 419990;
+    }
+    if (item == 'Watch SE') {
+        ar += 129990;
+    }
+    listaszam += 1
+    if (listaszam == 1){
+        lista1.style.visibility = 'visible'
+        lista1.innerHTML = item
+    }
+    if (listaszam == 2){
+        lista2.style.visibility = 'visible'
+        lista2.innerHTML = item
+    }
+    if (listaszam == 3){
+        lista3.style.visibility = 'visible'
+        lista3.innerHTML = item
+    }
+    if (listaszam == 4){
+        lista4.style.visibility = 'visible'
+        lista4.innerHTML = item
+    }
+    if (listaszam == 5){
+        lista5.style.visibility = 'visible'
+        lista5.innerHTML = item
+    }
+    if (listaszam == 6){
+        lista6.style.visibility = 'visible'
+        lista6.innerHTML = item
+        listaszam = 0
+    }
+    console.log(listaszam)
+    document.getElementById('osszesar').innerHTML = 'Ár: ' + ar
+} 
